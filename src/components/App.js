318ca4c8ac8caaa6d567997dc8192935a5ff007b
David <davidlopena@outlook.com>
2018-05-10 01:26:50 +0800

    Added eslint-config-airbnb, eslint-config-prettier and eslint-plugin-react.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
+
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
   </div>
-)
-export default App
+);
+export default App;
